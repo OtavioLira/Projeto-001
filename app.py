bc451cea58d0d90649aeafcfa398117cf2500eda
OtavioLira <otavioliraneves@gmail.com>
2024-12-04 22:37:39 -0300

    fix - undo port variable

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask
 from src.routes.cid_routes import init_routes
-import os
-from dotenv import load_dotenv
 
 app = Flask(__name__)
-
-load_dotenv()
-
-PORT = os.environ["PORT"]
 
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, PORT=PORT)
+    app.run(debug=True)
