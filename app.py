3ebe3bb12d54d8b458257254b4e1f8e18f4dddb3
OtavioLira <otavioliraneves@gmail.com>
2024-12-04 22:06:03 -0300

    feat - port variable

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from src.routes.cid_routes import init_routes
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
+
+PORT = os.environ["PORT"]
 
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, PORT=PORT)
